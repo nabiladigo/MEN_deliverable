0b416f7b8dcaef4fcdbea8bc13b7f906c7143564
nabiladigo <nabiladigo91@gmail.com>
2022-01-21 20:14:56 -0500

    update the index and create route redirect to index

--- models/music_model.js
+++ models/music_model.js
@@ -1,23 +1,148 @@
-const mongoose = require('mongoose');
-
-const musicSchema = new mongoose.Schema({
-    song: {
-        type: String,
-        required: true
-    },
-    singer: {
-        type: String,
-        
-        required: true,
-    },
-    image: {
-        type: String,
-        required: [true, "image can not be empty"],
-    },
-    date: {
-        type: String,
+class Collection {
+    #Model
+    #currentId
+    #items
+    constructor(model, startingData) {
+        this.#Model = model;
+        this.#currentId = 0;
+        this.#items = this.#populateItems( startingData );
     }
-});
 
-const Music = mongoose.model('Music', musicSchema);
-module.exports = Music;
+    /**
+     * @description It will take an array as a argument 
+     * @returns on Object that contains the { id as a key } and { te item as the value } 
+     */
+
+    #populateItems( startingData ) {
+        return startingData.reduce(( acc, item, idx ) => {
+            this.#currentId = idx;
+            acc[this.#currentId] = new this.#Model(item, idx)
+            return acc;
+        }, {});
+    }
+
+    #generateId(){
+        return ++this.#currentId
+    }
+
+    /**
+     * @description Will return an array with all items availible in this.items
+     * @returns array
+     */
+
+    find() {
+        return Object.values(this.#items);
+    }
+
+    /**
+     * @description Will return item match with the itemId
+     * @param { string } itemId
+     * @param { function } callBack Will return error or item
+     * @returns function;
+     */
+
+    findById( itemId, callBack ) {
+        if (!itemId) return console.log("missing id in first argument");
+    
+        if (typeof callBack !== "function") {
+            return console.log("missing function in second argument");
+        }
+    
+        let error;
+        const item = this.#items[itemId];
+    
+        if (!item) {
+            error = { message: `item with id "${itemId}" can't be found` };
+        }
+    
+        return callBack(error, item);
+    }
+
+    create( data, callBack ) {
+        if (!data) return console.log("missing data in first argument");
+    
+        if (typeof callBack !== "function") {
+            return console.log("missing function in second argument");
+        }
+    
+        let error, newItem;
+    
+        const isEmpty = Object.keys(data).every(field => data[field] === "");
+    
+        if (isEmpty) {
+            error = { message: `you have empty fields` };
+        } else {
+            
+            newItem = new this.#Model( data, this.#generateId());
+    
+            this.#items[newItem.id] = newItem;
+        }
+    
+        return callBack(error, newItem);
+    }
+    
+    findByIdAndDelete( itemId, callBack ) {
+        let error = null;
+        const item = this.#items[itemId]
+        const isDeleted = delete this.#items[itemId];
+    
+        if ( !isDeleted ) {
+          error = { message: `item with id "${itemId}" can't be found` };
+        }
+    
+        return callBack(error, item);
+     }
+     findByIdAndUpdate( itemId, data, callBack ) {
+        let error = null;
+        const item = this.#items[itemId];
+    
+        if (!item) {
+            error = { message: `item can't be found` };
+        } else {
+            this.#items[itemId] = {
+                ...item,
+                ...data
+            }
+        }
+    
+        return callBack(error, this.#items[itemId]);
+    }
+};
+
+class Song {
+    constructor( data, id ) {
+        this.id = id;
+        this.song = data.song;
+        this.image = data.image;
+        this.singer = data.singer;
+        this.date= data.date;
+    }
+}
+
+module.exports = new Collection(Song, [
+    {
+        song:"Hello",
+        singer:"Beyonce",
+        image: "https://upload.wikimedia.org/wikipedia/en/a/ac/Beyonce_-_Halo.png",
+        date: "2014"
+    },
+    {
+        song:"Bad Habits",
+        singer:"Ed Sheeran",
+        image: "https://img.youtube.com/vi/orJSJGHjBLI/hqdefault.jpg",
+        date: "2021"
+    },
+    {
+        song:"J'en ai marre",
+        singer:"Najat Aatabou-نجاة اعتابو",
+        image: "https://static.infofamouspeople.com/avatar/bn3fp1r2a2a8t90v86t0_faces_aatabou-najat-image.jpg",
+        date: "2021"
+    },
+    {
+        song:"Twinkle Little Star",
+        singer:"Baby Lullabies",
+        image: "https://i.ytimg.com/vi/-JRJibhgwUQ/maxresdefault.jpg",
+        date: "2021"
+    }
+
+]);
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express =require('express');
 const app = express();
+// const methodOverride = require('method-override');
+// const controllers = require('./controllers');
+
 const PORT = 5000;
-const music = require('./models/music_model.js');
 
 
-require('./config/db.connection.js');
-
-app.set('view engine', 'ejs');
-// middleware
-app.use(express.static('puplic'));
+// app.set('view engine', 'ejs');
+// // middleware
+// app.use(express.static('puplic'));
 app.use(express.urlencoded({extended: false}));
+// app.use(methodOverride('_method'))
+// app.use('/songs', controllers.song);
 
 app.use((req, res, next)=>{
     console.log(`${req.method} ${req.originalUrl}`);
@@ -19,23 +21,49 @@ app.use((req, res, next)=>{
 app.get('/', (req, res)=>{
     res.send('hello world');
 });
+app.post('/songs/', (req, res) => { 
+	res.send('hi');
+});
 
+app.get('/songs', (req, res) => {
+    // const allSongs = songs.find();
+    // Song.find({}, (error, foundSong) => {
+    //     if(error) return console.log(error);
 
-app.get('/new', (req, res)=>{
+    //     console.log(foundSong)
+    //     context = {songs: foundSong
+    //     }
+        res.render('index.ejs');
+        
+   
+});
+app.get('/songs/new', (req, res)=>{
     res.render('new.ejs');
 });
-app.get('/music', (req, res)=>{
-    res.render('index.ejs');
-});
 
-app.post('/music', (req, res) =>{
-    music.create(req.body, (error, createdMusic) =>{
+app.post('/songs', (req, res) =>{
+    songs.create(req.body, (error, createdSong) =>{
         if(error) return console.log(error);
-
-         console.log(createdMusic);
-        res.redirect('/music');
+         console.log(createdSong);
+        res.redirect('/songs');
     })
 })
+
+app.get('/:songId', (req, res) => {
+    
+    songs.findById(req.params.musicId, (error, foundSong) => {
+        if (error) {
+            console.log(req.params)
+            console.log(error);
+            const context = { error: error };
+            return res.status(404).render("404", context);
+        }
+       
+        res.render('show.ejs');
+    });
+    
+});
+
 
 app.get("/*", (req, res) => {
     const context = { error: req.error };
